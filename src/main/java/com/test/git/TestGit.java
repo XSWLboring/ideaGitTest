9211e3f33d8cae703ae1be5f3a6c0656839bd5c4
wbz <wbz@1027543292.com>
2020-08-01 12:30:14 +0800

    创建分支

--- src/main/java/com/test/git/TestGit.java
+++ src/main/java/com/test/git/TestGit.java
@@ -1,6 +1,8 @@
 package com.test.git;
 
 
+
+
 public class TestGit {
 
     public  static  void  main(String args[]){
@@ -9,5 +11,6 @@ public class TestGit {
         System.out.println("更新2");
         System.out.println("更新3");
         System.out.println("更新5");
+        System.out.println("创建分支");
     }
 }
