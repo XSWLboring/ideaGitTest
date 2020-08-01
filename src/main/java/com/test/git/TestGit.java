a87133f564b8f025d451970ee43445447b79495a
wbz <wbz@1027543292.com>
2020-08-01 10:58:27 +0800

    Second Commit

--- src/main/java/com/test/git/TestGit.java
+++ src/main/java/com/test/git/TestGit.java
@@ -6,5 +6,6 @@ public class TestGit {
     public  static  void  main(String args[]){
         System.out.println("addd");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
